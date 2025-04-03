20ff1c6191b29c5a981e629ec50dd21294c85f30
DegoVu44 <diegopinelli7@gmail.com>
2025-04-03 09:22:04 -0300

    Update whatsauto_reddit_bot.py

--- whatsauto_reddit_bot.py
+++ whatsauto_reddit_bot.py
@@ -188,7 +188,8 @@ def generate_imei():
     return jsonify({'error': 'Formato incorrecto. Usa: f4 IMEI, bl IMEI o fmi IMEI'}), 400
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna dinámicamente el puerto
+    app.run(host="0.0.0.0", port=port)
 
 
 
