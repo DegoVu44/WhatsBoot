0cc5f952b1407ef7ab546150da937e45bbf1cc09
DegoVu44 <diegopinelli7@gmail.com>
2025-04-08 22:32:16 -0300

    Update whatsauto_reddit_bot.py

--- whatsauto_reddit_bot.py
+++ whatsauto_reddit_bot.py
@@ -218,14 +218,14 @@ def generate_imei():
         print(f"Comando 'check' recibido con dato: {imei}")
 
         if imei.isdigit() and is_valid_imei(imei):
-            print(f"IMEI válido detectado: {imei}")
-        elif is_valid_serial(imei):
-            print(f"Serial válido detectado: {imei}")
-        else:
-            print(f"Ni IMEI ni Serial válidos: {imei}")
-            return jsonify({'reply': "❌ *Los datos Ingresados no corresponden a una orden valida verifica porfavor.*"}), 200
+    print(f"IMEI válido detectado: {imei}")
+elif is_valid_serial(imei):
+    print(f"Serial válido detectado: {imei}")
+else:
+    print(f"Ni IMEI ni Serial válidos: {imei}")
+    return jsonify({'reply': "❌ *Los datos Ingresados no corresponden a una orden valida verifica porfavor.*"}), 200
 
-        response_message, success = check_full_imei_details(imei)
+response_message, success = check_full_imei_details(imei)  # Acá se manda a la API
 
         if success:
             return jsonify({'reply': response_message}), 200
